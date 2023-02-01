04e8712471afa7fab21f2935bb3cb3a047d62f62
Sabrina <sabrinabuenoprata@gmail.com>
2023-02-01 20:09:40 -0300

    adição exercicios de aula

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Brasil";
+let continent = "America do Sul";
+let population = "214,3 Milhões";
+
+console.log("Brasil");
+console.log("America do sul");
+console.log("214,3 Milhões");
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,16 @@
 let js = "amazing";
-if  (js === "amazing") alert("JavaScript is FUN");
-
 
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas"
+
+let javascriptIsFun = true;
+console.log (javascriptIsFun);
+
+console.log (typeof true);
+console.log (typeof javascriptIsFun);
+console.log (typeof 23);
+console.log (typeof "Jonas");
