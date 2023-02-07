9a343204e4403663548876eb53a31c05203af332
Sabrina <sabrinabuenoprata@gmail.com>
2023-02-07 13:51:47 -0300

    Novos exercícios de aula

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,8 @@
-let country = "Brasil";
+/*let country = "Brasil";
 let continent = "America do Sul";
 let population = "214,3 Milhões";
 
 console.log("Brasil");
 console.log("America do sul");
 console.log("214,3 Milhões");
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/* let js = "amazing";
 
 console.log(40 + 8 + 23 - 10);
 
@@ -14,3 +14,95 @@ console.log (typeof true);
 console.log (typeof javascriptIsFun);
 console.log (typeof 23);
 console.log (typeof "Jonas");
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990
+
+var job = 'programmer';
+job = 'teacher'
+
+/*lastName = 'schmedtmann';
+console.log(lasName); */
+
+// Math operators
+/* const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas/10, 2**3)
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lasName = 'Schmedtmann';
+console.log(firstName + ' ' + lasName);
+
+// Assignment operators
+let x = 10 +5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++ ; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);// >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+// operator precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+// Coding challeng #1
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// Strings and template Literals
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas)
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew)
+
+console.log(`String
+multiple
+lines`)
+
+*/
+
+// Taking Decisions: if / else statements
+
+ 
