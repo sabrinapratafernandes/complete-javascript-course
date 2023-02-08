90fe9e6580dc267582e62c7bc279802577203865
Sabrina <sabrinabuenoprata@gmail.com>
2023-02-08 17:56:25 -0300

    Pratica em aula, if e else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -103,6 +103,25 @@ lines`)
 
 */
 
-// Taking Decisions: if / else statements
+// Taking Decisions: if / else statement
 
- 
+const  age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving licence');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)` );
+}
+
+const birthYear = 1991;
+
+let century;
+
+if (birthYear <= 2000 ) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century)
